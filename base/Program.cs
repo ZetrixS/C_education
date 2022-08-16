17b4990a12f18b776b9d8884518e7bf8020bc99b
unknown <zetriss@gmail.com>
2022-08-17 01:22:45 +0300

    base_test

---
+++ base/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 2;
+int b = 4;
+int c = 1;
+int d = 8;
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+ Console.Write("max = ");
+ Console.WriteLine(max);
